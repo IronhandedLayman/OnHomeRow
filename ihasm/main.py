76a93c5568b27d989658080057a6dea91e49261c
IronhandedLayman <nathaniel.dean@alumni.purdue.edu>
2022-02-16 23:13:11 -0500

    Episode 6 code: first steps towards writing an assembler! Exciting times.

--- ihasm/main.py
+++ ihasm/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
+import pyparsing as pp
+
 VERSION="0.0"
 
 PARSER_LANG="""
-TODO:    pragmas like #arch SBNO 
-TODO:    comments like // This is based on the SBN architecture indicated on the page
+#arch SBNO 
+TODO:    ; this is a comment in a mature assembler
 TODO:    section commands like .data test1
 TODO:    binary data lines like  0x 01 10 FE F6 00 a5 a5 a5
 TODO:    ascii data lines like "hi there"
@@ -12,8 +14,29 @@ TODO:    code lines like SBN a, b
 TODO:    inline expressions and pre-calculated values
 """
 
+PRAGMA_START=pp.Literal("#")
+PRAGMA_IDENTIFIER=pp.Word(pp.alphas)("pragma_identifier")
+PRAGMA_VALUE=pp.Word(pp.alphas)("pragma_value")
+
+PRAGMA=pp.Combine(PRAGMA_START+PRAGMA_IDENTIFIER)+PRAGMA_VALUE
+
+COMMENT_START = pp.Literal(";")
+COMMENT_LINE = COMMENT_START + pp.Group(pp.SkipTo(pp.LineEnd()))("comment") + pp.LineEnd()
+
+IHASM_LINE = pp.OneOrMore(PRAGMA | COMMENT_LINE)
+
 def main():
     print(f"ihasm version {VERSION}")
+    demo()
+
+def demo():
+    for input_line in [
+            "#arch SBNO",
+            "; this is just a comment",
+            ".code not_implemented_yet",
+            ]:
+        ihasm_ast = IHASM_LINE.parse_string(input_line)
+        print(repr(ihasm_ast))
 
 if __name__ == "__main__":
     main()
